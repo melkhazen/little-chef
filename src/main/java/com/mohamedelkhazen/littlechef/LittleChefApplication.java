637bd4862fd53a3eeba7f953548347d40d7213ee
mohamed <elkhazen1@gmail.com>
2021-06-18 12:13:23 -0500

    first commit

---
+++ src/main/java/com/mohamedelkhazen/littlechef/LittleChefApplication.java
@@ -0,0 +1,13 @@
+package com.mohamedelkhazen.littlechef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleChefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohamedelkhazen/littlechef/LittleChefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamedelkhazen.littlechef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
